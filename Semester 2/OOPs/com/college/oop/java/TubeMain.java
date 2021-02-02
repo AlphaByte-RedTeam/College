3ef6eb6e8a3e039191f3febdeeba5de6e8ab2b33
Andrew Virya Victorio <andrew.avv03@gmail.com>
2021-02-02 10:34:47 +0700

    Add files via upload

---
+++ Semester 2/OOPs/com/college/oop/java/TubeMain.java
@@ -0,0 +1,27 @@
+package com.college.oop.java;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class TubeMain {
+    public static void main(String[] args) {
+        // Making the constructor
+        Scanner input = new Scanner( System.in );
+        Formula obj = new Formula();
+
+        // Getting input from user
+        System.out.print( "Input the Area (cm): " );
+        obj.radius = input.nextInt();
+
+        System.out.print( "Input the height (cm): " );
+        obj.height = input.nextInt();
+
+        // Printing the result
+        System.out.println( "The result is " + obj.tube() );
+
+        System.out.println( "The result is " + Math.round(obj.tube()) + " without decimal point." );
+
+        // Closing the input, otherwise the resource will get leaked.
+        input.close();
+    }
+}
