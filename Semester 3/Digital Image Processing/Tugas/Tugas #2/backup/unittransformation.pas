bc3ccde3f04a4db3386f6bc927c07ae1102d0a13
Andrew Virya Victorio <andrew.avv03@gmail.com>
2021-12-14 20:58:17 +0700

    refactor: add design interface

--- Semester 3/Digital Image Processing/Tugas/Tugas #2/backup/unittransformation.pas
+++ Semester 3/Digital Image Processing/Tugas/Tugas #2/backup/unittransformation.pas
@@ -5,10 +5,30 @@ unit UnitTransformation;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls;
 
 type
-  TForm1 = class(TForm)
+
+  { TformTransformation }
+
+  TformTransformation = class(TForm)
+    btnRotate: TButton;
+    btnLoad: TButton;
+    btnSave: TButton;
+    btnMove: TButton;
+    btnScale: TButton;
+    imgSrc: TImage;
+    imgMod: TImage;
+    txtNote: TLabel;
+    txtImgMod: TLabel;
+    txtImgOri: TLabel;
+    txtYVal: TLabel;
+    txtXVal: TLabel;
+    editX: TEdit;
+    editY: TEdit;
+    openDialog: TOpenDialog;
+    saveDialog: TSaveDialog;
+    procedure btnLoadClick(Sender: TObject);
   private
 
   public
@@ -16,11 +36,32 @@ type
   end;
 
 var
-  Form1: TForm1;
+  formTransformation: TformTransformation;
 
 implementation
 
 {$R *.lfm}
+
+{ TformTransformation }
+
+procedure TformTransformation.btnLoadClick(Sender: TObject);
+var
+  x, y: integer;
+begin
+  if (openDialog.Execute) then
+  begin
+    imgSrc.Picture.LoadFromFile(openDialog.FileName);
+    for y:=0 to imgSrc.Height-1 do
+    begin
+      for x:=0 to imgSrc.Widht-1 do
+      begin
+        bmpR[x,y] := getRValue(imgSrc.Canvas.Pixels[x,y]);
+        bmpG[x,y] := getGValue(imgSrc.Canvas.Pixels[x,y]);
+        bmpB[x,y] := getBValue(imgSrc.Canvas.Pixels[x,y]);
+      end;
+    end;
+  end;
+end;
 
 end.
 
