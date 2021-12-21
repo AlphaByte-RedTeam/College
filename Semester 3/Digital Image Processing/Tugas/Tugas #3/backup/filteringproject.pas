1236482ae4859b773feff8cc2b43e3a5ec277729
Andrew Virya Victorio <andrew.avv03@gmail.com>
2021-12-22 06:55:13 +0700

    add: 3 button
    - load
    - save
    - process

--- Semester 3/Digital Image Processing/Tugas/Tugas #3/backup/filteringproject.pas
+++ Semester 3/Digital Image Processing/Tugas/Tugas #3/backup/filteringproject.pas
@@ -5,16 +5,10 @@ unit FilteringProject;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
 
 type
-
-  { TForm1 }
-
   TForm1 = class(TForm)
-    btnLoad: TButton;
-    btnSave: TButton;
-    btnProcess: TButton;
   private
 
   public
