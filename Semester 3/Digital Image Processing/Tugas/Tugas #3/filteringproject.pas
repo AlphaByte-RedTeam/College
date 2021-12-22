ebf93d8fe99138a7d497b888701a8dfe8271cdce
Andrew Virya Victorio <andrew.avv03@gmail.com>
2021-12-22 07:25:40 +0700

    update: make load button functional

--- Semester 3/Digital Image Processing/Tugas/Tugas #3/filteringproject.pas
+++ Semester 3/Digital Image Processing/Tugas/Tugas #3/filteringproject.pas
@@ -24,6 +24,7 @@ type
     radioGroupColor: TRadioGroup;
     radioGroupFilter: TRadioGroup;
     saveDialog: TSaveDialog;
+    procedure btnLoadClick(Sender: TObject);
   private
 
   public
@@ -36,6 +37,41 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TFilteringForm }
+
+uses Windows;
+
+var
+  bmpR, bmpG, bmpB, bmpGray: array[0..1000, 0..1000] of byte;
+  __initWidth__, __initHeight__: integer;
+
+procedure TFilteringForm.btnLoadClick(Sender: TObject);
+var
+  x, y: integer;
+  gray: byte;
+begin
+  if openDialog.Execute then
+  begin
+    imgOriginal.Picture.LoadFromFile(openDialog.FileName);
+
+    __initWidth__ := imgOriginal.Width;
+    __initHeight__ := imgOriginal.Height;
+
+    for y:=0 to imgOriginal.Height-1 do
+    begin
+      for x:=0 to imgOriginal.Width-1 do
+      begin
+        bmpR[x,y] := getRValue(imgOriginal.Canvas.Pixels[x,y]);
+        bmpG[x,y] := getGValue(imgOriginal.Canvas.Pixels[x,y]);
+        bmpB[x,y] := getBValue(imgOriginal.Canvas.Pixels[x,y]);
+
+        gray := (bmpR[x,y] + bmpG[x,y] + bmpB[x,y]) div 3;
+        bmpGray[x,y] := gray;
+      end;
+    end;
+  end;
+end;
 
 end.
 
