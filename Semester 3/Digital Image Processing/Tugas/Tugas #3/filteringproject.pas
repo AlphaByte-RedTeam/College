4b200209d0188d046d96a0801fcad20dc8e0f6eb
Andrew Virya Victorio <andrew.avv03@gmail.com>
2021-12-26 17:09:42 +0700

    add: new project

---
+++ Semester 3/Digital Image Processing/Tugas/Tugas #3/filteringproject.pas
@@ -0,0 +1,26 @@
+unit FilteringProject;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TFilteringForm = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  FilteringForm: TFilteringForm;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
