065e97df81d7444314842ba9a5f3dd99d251a6f1
Andrew Virya Victorio <andrew.avv03@gmail.com>
2021-12-27 13:35:05 +0700

    update: make button load functional

--- Semester 3/Digital Image Processing/Tugas/Tugas #4/arithmeticproject.pas
+++ Semester 3/Digital Image Processing/Tugas/Tugas #4/arithmeticproject.pas
@@ -19,7 +19,7 @@ type
     imgB: TImage;
     imgMod: TImage;
     openDialog: TOpenDialog;
-    RadioGroup1: TRadioGroup;
+    radioLoad: TRadioGroup;
     saveDialog: TSaveDialog;
     procedure btnLoadClick(Sender: TObject);
   private
@@ -39,6 +39,47 @@ implementation
 
 uses
   windows, math;
+
+var
+  bmpR, bmpG, bmpB: array[0..1000, 0..1000] of byte;
+
+procedure TformArithmetic.btnLoadClick(Sender: TObject);
+var
+  x, y: integer;
+begin
+  if radioLoad.ItemIndex = 0 then
+  begin
+    if openDialog.Execute then
+    begin
+      imgA.Picture.LoadFromFile(openDialog.FileName);
+      for y:=0 to imgA.Height-1 do
+      begin
+        for x:=0 to imgA.Width-1 do
+        begin
+          bmpR[x,y] := Red(imgA.Canvas.Pixels[x,y]);
+          bmpG[x,y] := Green(imgA.Canvas.Pixels[x,y]);
+          bmpB[x,y] := Blue(imgA.Canvas.Pixels[x,y]);
+        end;
+      end;
+    end;
+  end
+  else
+  begin
+    if openDialog.Execute then
+    begin
+      imgB.Picture.LoadFromFile(openDialog.FileName);
+      for y:=0 to imgB.Height-1 do
+      begin
+        for x:=0 to imgB.Width-1 do
+        begin
+          bmpR[x,y] := Red(imgB.Canvas.Pixels[x,y]);
+          bmpG[x,y] := Green(imgB.Canvas.Pixels[x,y]);
+          bmpB[x,y] := Blue(imgB.Canvas.Pixels[x,y]);
+        end;
+      end;
+    end;
+  end;
+end;
 
 end.
 
